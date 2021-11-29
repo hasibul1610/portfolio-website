4d349f47bc7fc35a8e5dba8e9b987ad722ca2751
hasibul1610 <bcse.hasibul@gmail.com>
2021-11-29 15:50:14 +0600

    first commit

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './Banner.css';
+import bannerBg from '../../../images/banner-bg.jpg';
+import { Container } from 'react-bootstrap';
+import Typewriter from 'typewriter-effect';
+import { Link } from 'react-router-dom';
+import resume from '../../../resume/resume.pdf';
+
+const bannerBgStyle = {
+    // backgroundImage: `url(${bannerBg})`,
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.557),rgba(0, 0, 0, 0.7)) , url(${bannerBg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+
+
+}
+
+const Banner = () => {
+    return (
+        <div style={bannerBgStyle}>
+            <Container>
+
+                <div className="banner d-flex align-items-center">
+                    <div className="banner-details">
+                        <p>HI THERE, I'M</p>
+
+                        <h3>
+                            <Typewriter
+
+                                options={{
+                                    strings: [' Md Hasibul Islam'],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                        </h3>
+                        <h4>Junior Web Developer</h4>
+                        <small>Experienced in HTML5, CSS3, Bootstrap5, React. Enthusiastic about MongoDb, Node, Express, Redux, React
+                            Native. Accustomed with Git, Github, Chrome Dev Tool, VS Code, Heroku, and MongoDB Atlas tools.</small>
+                        <br />
+                        <Link to={resume} download><button>My Resume  </button></Link>
+
+                    </div>
+
+                </div>
+
+            </Container>
+
+        </div>
+    );
+};
+
+export default Banner;
